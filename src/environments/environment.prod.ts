1619909ea7a46e6b0834269dc47d4ac2c3d47286
Hakeem <hakeem@alpha2k.io>
2021-03-15 00:58:34 -0400

    firebase configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAdm10ysd8FZb7qNXUEDj12KdN2WUR_9p4",
+    authDomain: "sherpa-assignment.firebaseapp.com",
+    projectId: "sherpa-assignment",
+    storageBucket: "sherpa-assignment.appspot.com",
+    messagingSenderId: "951936834198",
+    appId: "1:951936834198:web:811f362c4e2994a645c0b7",
+    measurementId: "G-JS24C9RDYB"
+  }
 };
